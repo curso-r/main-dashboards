07fbe4dd46154eab2876938ee5d3e9c546d103e6
williamorim <thewilliam89@gmail.com>
2020-06-12 21:03:26 -0300

    aula 6

--- rmarkdown/tabelas.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-imdb <- read_rds("dados/imdb.rds")
-
-# Tabela 1
-
-imdb %>% 
-  filter(diretor == "Quentin Tarantino") %>%
-  arrange(ano) %>%
-  select(`Título` = titulo, Ano = ano)
