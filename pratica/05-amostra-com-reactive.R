029384a05ce90f8ae1b9acd3816afa78ffee85a7
williamorim <thewilliam89@gmail.com>
2021-09-15 20:38:58 -0300

    revisando aula 2

---
+++ pratica/05-amostra-com-reactive.R
@@ -0,0 +1,35 @@
+library(shiny)
+
+ui <- fluidPage(
+  "Resultado do sorteio",
+  sliderInput(
+    inputId = "tamanho",
+    label = "Selecione o tamanho da amostra",
+    min = 1,
+    max = 1000,
+    value = 100
+  ),
+  plotOutput(outputId = "grafico"),
+  textOutput(outputId = "resultado")
+)
+
+server <- function(input, output, session) {
+  
+  amostra <- reactive({
+    sample(1:10, input$tamanho, replace = TRUE)
+  })
+  
+  output$grafico <- renderPlot({
+    amostra() |> 
+      table() |> 
+      barplot()
+  })
+  
+  output$resultado <- renderText({
+    contagem <- table(amostra())
+    mais_freq <- names(contagem[which.max(contagem)])
+    glue::glue("O valor mais sorteado foi o {mais_freq}.")
+  })
+}
+
+shinyApp(ui, server)
