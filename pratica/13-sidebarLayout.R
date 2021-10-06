c738ff594b50ddd3e8a9ab0d606de770d30457b3
williamorim <thewilliam89@gmail.com>
2021-10-06 20:27:36 -0300

    revisao aula 3

---
+++ pratica/13-sidebarLayout.R
@@ -0,0 +1,72 @@
+library(shiny)
+
+dados <- readr::read_rds("../dados/pkmn.rds")
+
+ui <- fluidPage(
+  titlePanel("Shiny com sidebarLayout"),
+  sidebarLayout( 
+    sidebarPanel(
+      selectInput(
+        "pokemon",
+        label = "Selecione um Pokemon",
+        choices = unique(dados$pokemon)
+      )
+    ),
+    mainPanel(
+      fluidRow(
+        column(
+          offset = 4,
+          width = 4,
+          imageOutput("pkmn")
+          # uiOutput("pkmn")
+        )
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  output$pkmn <- renderImage({
+    
+    id <- dados |> 
+      dplyr::filter(pokemon == input$pokemon) |> 
+      dplyr::pull(id) |> 
+      stringr::str_pad(width = 3, side = "left", pad = "0")
+    
+    url <- glue::glue(
+      "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/{id}.png"
+    )
+    
+    arquivo <- tempfile(fileext = ".png")
+    download.file(url, arquivo)
+    
+    list(
+      src = arquivo,
+      alt = glue::glue("Quem é este pokemon? É o {input$pokemon}."),
+      width = 300
+    )
+    
+  }, deleteFile = TRUE)
+  
+  # output$pkmn <- renderUI({
+  #   
+  #   id <- dados |> 
+  #     dplyr::filter(pokemon == input$pokemon) |> 
+  #     dplyr::pull(id) |> 
+  #     stringr::str_pad(width = 3, side = "left", pad = "0")
+  #   
+  #   url <- glue::glue(
+  #     "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/{id}.png"
+  #   )
+  #   
+  #   img(
+  #     src = url,
+  #     alt = glue::glue("Quem é este pokemon? É o {input$pokemon}."),
+  #     width = 300
+  #   )
+  # })
+  
+}
+
+shinyApp(ui, server)
