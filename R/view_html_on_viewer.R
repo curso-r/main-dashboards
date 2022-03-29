537a73f025349abade63b50d7839666c14697503
williamorim <thewilliam89@gmail.com>
2022-03-29 16:24:21 -0300

    tirando material do dash2

--- R/view_html_on_viewer.R
+++
@@ -1,8 +0,0 @@
-view_html_on_viewer <- function(file) {
-  dir <- tempfile()
-  dir.create(dir)
-  htmlFile <- file.path(dir, "temp.html")
-  file.copy(from = file, to = htmlFile)
-  rstudioapi::viewer(htmlFile)
-  rstudioapi::navigateToFile(htmlFile)
-}
