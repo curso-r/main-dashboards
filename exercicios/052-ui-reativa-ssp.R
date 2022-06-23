247ad3fc6f341a453772b4a4f34c462ef97783d2
williamorim <thewilliam89@gmail.com>
2022-06-22 21:32:53 -0300

    colocando exercicios nos slides

--- exercicios/052-ui-reativa-ssp.R
+++ exercicios/052-ui-reativa-ssp.R
@@ -3,5 +3,5 @@
 # Construa um shiny app que possua um filtro de delegacia
 # que depende de um filtro de município
 # que depende de um filtro de região.
-#
+# 
 # Como output, faça uma série temporal do número de furtos.
